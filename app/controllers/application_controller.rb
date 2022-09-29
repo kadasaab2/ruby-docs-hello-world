9f3397f853692a408d9b7e66d42bfdf7252b2857
kadasaab2 <113411499+kadasaab2@users.noreply.github.com>
2022-09-29 10:09:27 +0530

    hewllo word22

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-      render html: "Hello world from Azure App Service on Linux!"
+      render html: "Hello world from Azure22 App Service on Linux!"
   end
 end
